69996a5fda48ba8524c11d5fc8834e6a9b5abb91
Cesar <cesarcmartinez@gmail.com>
2019-10-03 10:37:28 -0500

    first commit

---
+++ Leaflet_Step-1/static/js/config.js
@@ -0,0 +1,16 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiY2VzYXJtMDkwNyIsImEiOiJjazA5b3kxcGIwYjNoM2hueWQ2dDZramlzIn0.BFPCwN_6BaN4q1osTcXgKg";
+
+const attribution = `
+  Map data & copy;
+  <a href="https://www.openstreetmap.org/">
+    OpenStreetMap
+    </a>
+  contributors,
+    <a href="https://creativecommons.org/licenses/by-sa/2.0/">
+      CC-BY-SA
+    </a>
+    , Imagery ©
+  <a href="https://www.mapbox.com/">
+    Mapbox
+    </a>`;
